24f13874548d03de6637862d3b81202be75eff55
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-01 19:49:07 -0700

    User (admin) can add a problem through UI. #14

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddConceptForm from './admin/add-concept'
+import AddProblemForm from './admin/add-problem'
 
 const App = () => {
   return (
     <div>
       <AddConceptForm />
+      <AddProblemForm />
     </div>
   )
 }
