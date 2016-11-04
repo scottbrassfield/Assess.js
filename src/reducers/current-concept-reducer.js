028d60e19b7bb0b8a70d41453911c642d8e7fa93
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-03 17:10:28 -0700

    Removes extraneous actions and commented code. #17

--- src/reducers/current-concept-reducer.js
+++ src/reducers/current-concept-reducer.js
@@ -1,45 +1,23 @@
 import { combineReducers } from 'redux'
-
-import {
-  LOAD_CURRENT_CONCEPT,
-  UPDATE_CURRENT_CONCEPT
-  // UPDATE_ANSWER
-} from '../actions'
+import { LOAD_CURRENT_CONCEPT } from '../actions'
 
 const details = (state = {}, action) => {
   switch(action.type) {
     case LOAD_CURRENT_CONCEPT:
       return {...action.details}
-    case UPDATE_CURRENT_CONCEPT:
-      return { ...state, status: action.status}
     default:
       return state
   }
-
 }
-
-// const updateProblem = (state = {}, action) => {
-//   switch(action.type) {
-//     case UPDATE_ANSWER:
-//       return { ...state, status: action.status }
-//     default:
-//       return state
-//   }
-// }
 
 const problems = (state = [], action) => {
   switch(action.type) {
     case LOAD_CURRENT_CONCEPT:
       return [...action.problems]
-    // case UPDATE_ANSWER:
-    //   return state.map((problem, i) => {
-    //     return (i === action.index) ? updateProblem(undefined, action) : problem
-    //   })
     default:
       return state
   }
 }
-
 
 export default combineReducers({
   details,
--- src/reducers/current-problem-reducer.js
+++ src/reducers/current-problem-reducer.js
@@ -1,8 +1,4 @@
-// import { combineReducers } from 'redux'
-
-import {
-  LOAD_CURRENT_PROBLEM,
-} from '../actions'
+import { LOAD_CURRENT_PROBLEM } from '../actions'
 
 const currentProblem = (state = {}, action) => {
   switch(action.type) {
@@ -14,20 +10,3 @@ const currentProblem = (state = {}, action) => {
 }
 
 export default currentProblem
-//
-// const status = (state = '', action) => {
-//   switch(action.type) {
-//     case UPDATE_CURRENT_PROBLEM:
-//       return state = action.status
-//     case LOAD_CURRENT_PROBLEM:
-//       return state = ''
-//     default:
-//       return state
-//   }
-// }
-//
-//
-// export default combineReducers({
-//   details,
-//   status
-// })
