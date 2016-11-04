eaa462223180cba77703202af81ad3e437d5db75
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-03 17:16:47 -0700

    Adds unstyled components for viewing and answering questions. #17

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import AddConceptForm from './admin/add-concept'
 import AddProblemForm from './admin/add-problem'
+import Assessment from './student/assessment'
 
 const App = () => {
   return (
     <div>
       <AddConceptForm />
       <AddProblemForm />
+      <div style={{marginTop: '30px'}} />
+      <Assessment />
     </div>
   )
 }
