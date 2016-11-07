423229526c240a33f7bcd13d3abd62746f56e3fa
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-07 08:31:34 -0800

    Adds routes for styled components.

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
-import Main from './main.js'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import Home from './home'
+import Main from './main/main'
 import Admin from './admin/admin'
 import Student from './student/student'
+import StudentDashboard from './student/student-dashboard'
+import AdminDashboard from './admin/admin-dashboard'
 import AddConceptForm from './admin/add-concept'
 import AddProblemForm from './admin/add-problem'
 import Assessment from './student/assessment'
 
-const App = () => {
-  return (
-    <Router history={hashHistory}>
-      <Route path='/' component={Main}>
-        <Route path='/admin' component={Admin} >
-          <Route path='/concept-form' component={AddConceptForm} />
-          <Route path='/problem-form' component={AddProblemForm} />
-        </Route>
-        <Route path='/student' component={Student}>
-          <Route path='/assessment' component={Assessment} />
-        </Route>
+const App = () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={Home} />
+    <Route path='/main' component={Main}>
+      <Route path='/admin' component={Admin} >
+        <IndexRoute component = {AdminDashboard} />
+        <Route path='/concept-form' component={AddConceptForm} />
+        <Route path='/problem-form' component={AddProblemForm} />
       </Route>
-      <AddConceptForm />
-      <AddProblemForm />
-      <div style={{marginTop: '30px'}} />
-      <Assessment />
-    </Router>
-  )
-}
+      <Route path='/student' component={Student}>
+        <IndexRoute component={StudentDashboard} />
+        <Route path='/assessment' component={Assessment} />
+      </Route>
+    </Route>
+  </Router>
+)
 
 export default App
